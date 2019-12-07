24647a5da4c6057b0aa760bc44ff7f2c6526f260
emilundgapp <lundgrenemil@hotmail.com>
2019-12-07 18:41:00 +0100

    SEPARATED image-handling scripts

--- resources/scripts/main.js
+++ resources/scripts/main.js
@@ -24,36 +24,6 @@ form.addEventListener('submit', function (e) {
     })
 })
 
-// This is needed in order to not store previous search results.
-function clearPreviousImages() {
-    while (galleryContainer.firstChild) {
-        galleryContainer.removeChild(galleryContainer.firstChild);
-    }
-}
-
-// This is needed in order to construct the correct image URL.
-function imageURLBuilder(imageObject) {
-    const imageFarm = imageObject.farm;
-    const imageServer = imageObject.server;
-    const imageID = imageObject.id;
-    const imageSecret = imageObject.secret;
-
-    const imagePath = 'https://farm' + imageFarm + '.staticflickr.com/' + imageServer + '/' + imageID + '_' + imageSecret + '.jpg';
-    return imagePath;
-}
-
-function displayImages(images) {
-    const galleryClasses = galleryContainer.classList;
-    const errorClass = "error-message__no-result";
-    if (galleryClasses.contains(errorClass)) {
-        galleryClasses.remove(errorClass);
-    };
-    images.forEach(imageSource => {
-        const imageElement = this.createImageElement(imageSource);
-        galleryContainer.appendChild(imageElement);
-    })
-}
-
 function displayEmpty() {
     const noResultText = document.createElement("h2");
     noResultText.textContent = "No images found! Try searching for something else.";
@@ -67,17 +37,4 @@ function displayEmpty() {
     galleryContainer
         .classList
         .add("error-message__no-result");
-}
-
-function createImageElement(imageSource) {
-    const imageElement = document.createElement("div");
-
-    // This is needed in order to add unique image source to a div.
-    imageElement.setAttribute("style", "background-image: url(" + imageSource + ");");
-
-    imageElement
-        .classList
-        .add('gallery__image')
-
-    return imageElement;
 }
