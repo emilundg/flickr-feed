14319fee83b4a107bd6ea99de730c8dc5f0091b1
emilundgapp <lundgrenemil@hotmail.com>
2019-12-08 20:00:05 +0100

    ADD so gallery is not dependant of searchresult

--- src/resources/scripts/main.js
+++ src/resources/scripts/main.js
@@ -1,22 +1,31 @@
 // Defined globally in order for other files to access galleryContainer.
 const galleryContainer = document.getElementById('galleryContainer');
 
+// Set the gallery to something nice as the page loaded. 
+document.addEventListener("DOMContentLoaded", function () {
+    fetchGallery('gothenburg');
+});
+
+// This is needed in order for searched user tags.
 const form = document.querySelector('form');
 form.addEventListener('submit', function (e) {
     e.preventDefault();
+    const searchedValue = document
+        .getElementById('searchInput')
+        .value;
 
+    fetchGallery(searchedValue);
+})
+
+function fetchGallery(searchParam) {
     // See ./loader.js for reference.
     displayLoader();
 
     // See ./image-handling.js for reference.
     clearPreviousImages();
 
-    const searchedValue = document
-        .getElementById('searchInput')
-        .value;
-
     // Fetched images is from API flickr-api.js.
-    let fetchedImages = fetchImages(searchedValue);
+    let fetchedImages = fetchImages(searchParam);
     fetchedImages.then(function (images) {
 
         // See ./loader.js for reference.
@@ -27,7 +36,7 @@ form.addEventListener('submit', function (e) {
             this.displayEmpty();
         }
     })
-})
+}
 
 // Needed in order to give feedback if the api call comes back with zero images.
 function displayEmpty() {
